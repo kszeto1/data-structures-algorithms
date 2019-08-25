1acd47b7c80cc704c14f12d8c0bbed61bb74bf9d
Kenny Szeto <kenny.szeto1@gmail.com>
2019-08-25 16:34:24 -0700

    Added solution for TwoSum

--- TwoSum.js
+++ TwoSum.js
@@ -4,16 +4,27 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = [];
-    for (let num of nums) {
-        for (let i = 1; i < nums.length; i++) {
-            if (num + nums[i] === target) {
-                if (!result.includes(nums.indexOf(num)) && !result.includes(nums.indexOf(nums[i]))) {
-                    result.push(nums.indexOf(num), nums.indexOf(nums[i], nums.indexOf(nums[i])));
-                    break;
-                }
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] = target - nums[i]) {
+                return [i, j];
             }
         }
     }
-    return result;
 };
+
+
+// var twoSum = function(nums, target) {
+//     let result = [];
+//     for (let num of nums) {
+//         for (let i = 1; i < nums.length; i++) {
+//             if (num + nums[i] === target) {
+//                 if (!result.includes(nums.indexOf(num)) && !result.includes(nums.indexOf(nums[i]))) {
+//                     result.push(nums.indexOf(num), nums.indexOf(nums[i], nums.indexOf(nums[i])));
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     return result;
+// };
