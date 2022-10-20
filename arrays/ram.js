b22b9679e6ed20d292a7ec35cd1cbfc0a1c1c13b
Kenny <kenny.szeto1@gmail.com>
2022-10-19 20:16:22 -0700

    Neetcode setup / ram

---
+++ arrays/ram.js
@@ -0,0 +1,18 @@
+/*
+computer usually has 8 GB of ram
+1 byte = 8 bits
+1 bit = 0 or 1
+
+[1, 3, 5]
+integer 1 is stored in bytes, commonly 4 bytes (32 bits) ________________________________
+it would be 31 zeroes and a 1 at the end 00000000000000000000000000000001
+
+RAM
+value  ____ 1  3  5 ________
+address____$0_$4_$8
+
+address incremented 4 bytes
+ASCII characters take only 1 byte
+____$1_$2_$3
+
+*/
