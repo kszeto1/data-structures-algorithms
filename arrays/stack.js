6faa205dae70c326334e4320b24423d09d584a0a
kszeto1 <kenny.szeto1@gmail.com>
2024-08-13 17:33:21 -0700

    217. Contains Duplicate

---
+++ arrays/stack.js
@@ -0,0 +1,12 @@
+// stacks
+// pop - O(1)
+//  push - O(1)
+// peek/top - O(1)
+// in javascript, this is just a dynamic array. don't need to build out a new data structure
+// 
+// const myArr = []
+// LIFO data structure
+// can be used to reverse a sequence
+// 
+// 
+// 
---
+++ leetcode/217-contains-duplicate.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+// 217. Contains Duplicate
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+
+var containsDuplicate = function(nums) {
+    // try using js Map()
+    const freq = new Map();
+
+    for (const num of nums) {
+        if (freq.has(num)) return true; // O(1) time complexity for retrieval of value
+        freq.set(num, 1) //O(1) time complexity for setting value
+    }
+
+    return false;
+};
