f81ef51b5df146e6079bd38539c2210ca1fd7034
kszeto1 <kenny.szeto1@gmail.com>
2024-08-22 00:19:40 -0700

    136. Single Number

--- leetcode/136-single-number.js
+++ leetcode/136-single-number.js
@@ -41,3 +41,7 @@ var singleNumber = function(nums) {
         if (freq[key] === 1) return key
     }
 };
+
+console.log(singleNumber([2,2,1]))
+console.log(singleNumber([4,1,2,1,2]))
+console.log(singleNumber([1]))
